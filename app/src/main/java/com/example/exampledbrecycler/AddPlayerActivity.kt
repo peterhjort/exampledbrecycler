c514da8ed1d428e0f2f308d010ca5c21bf2f3282
Peter Hjort <peter.k.hjort@gmail.com>
2021-02-11 14:21:38 +0200

    Step 2: addplayer

---
+++ app/src/main/java/com/example/exampledbrecycler/AddPlayerActivity.kt
@@ -0,0 +1,52 @@
+package com.example.exampledbrecycler
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.exampledbrecycler.databinding.ActivityAddPlayerBinding
+import kotlinx.coroutines.launch
+
+class AddPlayerActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddPlayerBinding
+    private lateinit var viewModel: AddPlayerViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_add_player)
+        viewModel = ViewModelProvider(this).get(AddPlayerViewModel::class.java)
+
+        binding.button.setOnClickListener {
+            if(binding.name.text.isNotEmpty()
+                && binding.team.text.isNotEmpty()
+                && binding.year.text.isNotEmpty()
+                && binding.points.text.isNotEmpty()) {
+                viewModel.addPlayer(
+                    binding.name.text.toString(),
+                    binding.team.text.toString(),
+                    binding.year.text.toString().toIntOrNull() ?: 0,
+                    binding.points.text.toString().toIntOrNull() ?: 0
+                )
+                Toast.makeText(applicationContext, "Player added.", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(applicationContext, "Not good.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
+
+class AddPlayerViewModel(application: Application) : AndroidViewModel(application) {
+    fun addPlayer(name: String, team: String, year: Int, points: Int) {
+        val context = getApplication<Application>().applicationContext
+        viewModelScope.launch {
+            PlayerDB.getInstance(context)
+                .playerDAO
+                .insertOrUpdate(Player(0, name, team, year, points))
+        }
+    }
+}
--- app/src/main/java/com/example/exampledbrecycler/MainActivity.kt
+++ app/src/main/java/com/example/exampledbrecycler/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.exampledbrecycler
 
+import android.app.Application
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.exampledbrecycler.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.options_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.addPlayer -> {
+                startActivity(Intent(this, AddPlayerActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
+
+class MainViewModel(application: Application) : AndroidViewModel(application)
